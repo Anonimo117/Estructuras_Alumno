964fb6351ec1f342e3e684c55d1158e72a764fca
Anonimo117 <alangarciac0516@gmail.com>
2021-02-14 23:29:13 -0600

    Tarea1_Alumno

---
+++ Estructuras.java
@@ -0,0 +1,17 @@
+
+package estructuras;
+
+/**
+ *
+ * primera tarea
+ */
+public class Estructuras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo ");
+    }
+    
+}
